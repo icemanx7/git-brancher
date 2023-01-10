d4477283cb5413fa071b05efd72c765f71b4eee6
Richard Thomas <richard.thomas36@gmail.com>
2023-01-10 16:07:03 +0200

    added initial code for generating the branch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct CliArguments {
+    ticket_number: String,
+    desciption: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: CliArguments = CliArguments::parse();
+    println!("{:?}", args);
+
+    let desc: String = args.desciption.to_lowercase().replace(" ", "-");
+
+    let branch: String = format!("{}-{}", args.ticket_number, desc);
+
+    println!("{}", branch)
 }
